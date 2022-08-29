819b09473ed1fa5eea18e0cb230391b32ffe8483
darius-dinu <darius.dinu2007@gmail.com>
2022-08-30 00:32:10 +0300

    Edited the variable file in the CPP script

--- test_variables.cpp
+++ test_variables.cpp
@@ -10,6 +10,6 @@ int main ()
     int varsta;
     varsta = 65;
     cout << "Hello everybody, I am " << nume << endl;
-    cout << "I am " << varsta << " years old and I like" << hobby << endl;
+    cout << "I am " << varsta << " years old and I like " << hobby << endl;
     return 0;
 }
