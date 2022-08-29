aaf29921d558e10d829c00b95c8cb63e74e065a4
darius-dinu <darius.dinu2007@gmail.com>
2022-08-30 00:32:55 +0300

    Added a data types file to the CPP script

---
+++ data_types.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char litera = 'D';
+    string propozitie = "El este Stefan.";
+    int a = 6;
+    float b = 5 / 3;
+    bool nebunie = false;
+
+}
