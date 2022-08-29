8af2e6e1cbcca1cfcaae951a7fdb2d680e60ae3d
darius-dinu <darius.dinu2007@gmail.com>
2022-08-29 22:49:50 +0300

    Added a variable story to the CPP script

---
+++ test_variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main ()
+{
+    string nume = "Buck";
+    string hobby = "football";
+    int varsta;
+    varsta = 65;
+    cout << "Hello everybody, I am " << nume << endl;
+    cout << "I am " << varsta << " years old and I like" << hobby << endl;
+    return 0;
+}
