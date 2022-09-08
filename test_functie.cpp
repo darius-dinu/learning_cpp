fa91b65909fc2de7fc2fda2204a49be084a98ff4
darius-dinu <darius.dinu2007@gmail.com>
2022-09-08 10:51:29 +0300

    Added a function test file to the CPP script

---
+++ test_functie.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void salut(string a)                        // Aceasta este o functie care primeste ca parametru un string
+{
+    cout << "Salutare, " << a << endl;      // Functia trebuie sa salute user-ul.
+}
+
+int main()                                  // In functie main, apelam functia salut       
+{
+    salut("Darius");                         // Apelarea functiei salut cu parametrul "Darius"
+    return 0;
+
+
+}
