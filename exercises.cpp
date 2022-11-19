110a277329abd98509583eb79e640a84a6d2b5b1
darius-dinu <darius.dinu2007@gmail.com>
2022-11-19 12:38:38 +0200

    Added exercises to the CPP script

--- exercises.cpp
+++ exercises.cpp
@@ -15,3 +15,59 @@ int main()
     }
     return 0;
 }   
+
+
+
+
+// 2. Write a C++ program that checks if a number is a super-palindrome. (A super-palindrome is a number that is a palindrome and also the square of the number is a palindrome)
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    long long number, temp, verificare, DigitsNumber = 0, invertN = 0, square, temp2, verificare2, DigitsSquare = 0, invertSquare = 0;
+    cin >> number;
+    temp = number;                       // Variable that is used for calculating the number of digits for the initial number.
+    verificare = number;                 // Variable that is used for checking.
+    square = number * number;
+    temp2 = square;                      // Variable that is used for calculating the number of digits for the square.
+    verificare2 = square;                // Variable that is used for checking.
+
+    while (temp)                         // While loop that calculates the number of digits for the initial number
+    {
+        DigitsNumber ++;
+        temp /= 10;
+
+    }
+    
+
+    while (number && DigitsNumber)                      // While loop that calculates the inverted of the initial number
+    {
+        invertN += (n % 10) * pow (10, DigitsNumber - 1);
+        number /= 10;
+        DigitsNumber --;
+    }
+    
+
+    while (temp2)                       // While loop that calculates the number of digits for the square
+    {
+        DigitsSquare++;
+        temp2 /= 10;
+
+    }
+
+    while (square && DigitsSquare)                      // While loop that calculates the inverted of the square
+    {
+        invertSquare += (square % 10) * pow (10, DigitsSquare - 1);
+        square /= 10;
+        DigitsSquare --;
+    }
+
+    if (verificare == invertN && verificare2 == invertSquare)       // If-Else statement that checks if the number is a palindrome
+        cout << "The number is a super-palindrome";
+    else
+        cout << "The number isn't a super-palindrome";
+}
