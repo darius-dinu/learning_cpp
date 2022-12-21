7ce5e8973643553012c5d9a4db0154b8e4850e2a
darius-dinu <darius.dinu2007@gmail.com>
2022-12-21 11:53:36 +0200

    Added an exponent function to the CPP script

--- test_for.cpp
+++ test_for.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
+
 using namespace std;
 
-// Acesta este un cod care cere utilizatorului sa introduca numere pana la aparatia lui 0. Codul va intoarce cate numere din cele introduse erau impare.
+// This code requires the user to write numbers until the appearance of 0. The code will return how many odd numbers have been written.
 
-int main(){
-    long long n = 1, c = 0;
-    for (int i = 0; n != 0;){
-        cin >> n;
-        if (n % 2 != 0){
-            c++;
-        }
-        if (!n){
+int main()
+{
+    long long number = 1, countOdd = 0;
+    for (int i = 0; number != 0;)
+    {
+        cin >> number;
+        if (number % 2 != 0)
+            countOdd++;
+        if (!number)
             break;
-        }
     }
-    cout << c;
+    cout << countOdd;
+    return 0;
+}
+
+
+
+
+// The code below is an exponent function that should return the power of a number to a certain exponent.
+
+
+int power(long long base, long long exp)
+{
+    long long result = 1;
+    for (int i = 0; i < exp; ++i)
+        result *= base;
+
+    return result;
+}
+
+int main()
+{
+    cout << power(3, 4);            // This will return 81.
     return 0;
 }
